23c96f2783f38819d164bf61198d481edebffbf5
Isabela Sobral <isabela.sobral@dbtlabs.com>
2022-05-09 18:24:17 +0000

    test

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -6,7 +6,7 @@ with source as (
 ),
 
 
-modela as (
+modele as (
 
     select
     
@@ -25,7 +25,7 @@ modela as (
 
 )
 
-select * from modela
+select * from modele
 
 {% do log("this is schema  "+schema, info=true) %}
 {% do log("this is target.schema "+ target.schema, info=true) %}
